f5b233c3d598ce7b9e17ec977064a7d8f24e9669
Ruslan Akberov <ruslanakberov@gmail.com>
2018-04-01 13:43:40 +0300

    Detail menu item screen implemented

--- RestaurantMenu/Controllers/MenuItemDetailViewController.swift
+++ RestaurantMenu/Controllers/MenuItemDetailViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUi()
     }
     
+    func updateUi() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        descriptionLabel.text = menuItem.description
+        addToOrderButton.layer.cornerRadius = 5.0
+    }
+    
+    @IBAction func addToOrderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
